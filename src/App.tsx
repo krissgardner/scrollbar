a69c921f8912180ab9321d4ea77b83a8a6febd11
Kriss Gardner <krissrobert12@gmail.com>
2024-07-10 23:10:26 +0300

    Minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import HorizontalScroll from './components/HorizontalScroll'
 function App() {
   return (
     <div className="App">
+      <h1>Horizontal Scroll</h1>
       <HorizontalScroll className="client-scroll-classname">
         <div style={{ width: 1000, height: 300 }} />
       </HorizontalScroll>
